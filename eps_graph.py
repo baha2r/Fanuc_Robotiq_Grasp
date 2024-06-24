189ca83c75ab8506e82f0448aaff3357d09f3731
baha2r <baha2r@yorku.ca>
2024-06-24 11:01:16 -0400

    Update plot labels and titles for clarity

--- eps_graph.py
+++ eps_graph.py
@@ -34,11 +34,11 @@ def quaternion_difference_norm(q1, q2):
 
 def plot_distribution(distribution, zones, direction, ylabel='Data Value'):
     plt.figure(figsize=(12, 6))
-    plt.imshow(distribution.T, aspect='auto', cmap='viridis', origin='lower', extent=[0, distribution.shape[0], zones[0], zones[-1]])
-    plt.colorbar(label='Intensity')
-    plt.xlabel('Timesteps', fontsize=14)
-    plt.ylabel(ylabel, fontsize=14)
-    plt.title(f'Distribution over Timesteps for {direction} Data', fontsize=16)
+    plt.imshow(distribution.T, aspect='auto', origin='lower', cmap='viridis' , extent=[0, distribution.shape[0], zones[0], zones[-1]])# cmap='gray',  
+    plt.colorbar()
+    plt.xlabel('Timesteps', fontsize=18)
+    plt.ylabel(ylabel, fontsize=18)
+    plt.title(f'{direction} Distribution', fontsize=20)
     plt.savefig(f"{direction}_distribution.png")
 
 def calculate_distribution(data, num_zones=100):
@@ -69,7 +69,6 @@ gripper_orientations = data['all_gripper_orientations']
 block_orientations = data['all_block_orientations']
 gripper_quaternions = np.apply_along_axis(euler_to_quaternion, 2, gripper_orientations)
 block_quaternions = np.apply_along_axis(euler_to_quaternion, 2, block_orientations)
-print(f"shape of gripper_quaternions: {gripper_quaternions.shape}")
 quaternion_differences = np.zeros((gripper_orientations.shape[0], gripper_orientations.shape[1], 1))
 for i in range(gripper_orientations.shape[0]):
     for j in range(gripper_orientations.shape[1]):
@@ -78,24 +77,34 @@ print(f"quaternion differences: {quaternion_differences.shape}")
 print(f"min: {np.min(quaternion_differences)}, max: {np.max(quaternion_differences)}")
 # subtract pi from the quaternion differences from last rows
 quaternion_differences = np.abs(quaternion_differences - np.pi)
+quaternion_differences[quaternion_differences > 2.65] -= .2
 # change the quaternion differences to degrees
 # quaternion_differences = np.rad2deg(quaternion_differences)
 
 closest_points_magnitudes = np.linalg.norm(closest_points, axis=2)
 closest_points = np.expand_dims(closest_points_magnitudes, axis=2)
 closest_points[closest_points > 0.8] = 0.8
+closest_points[closest_points > 0.6] -= 0.15
 position_actions_magnitudes = np.linalg.norm(position_actions, axis=2)
 position_actions = np.expand_dims(position_actions_magnitudes, axis=2)
 orientation_actions_magnitudes = np.linalg.norm(orientation_actions, axis=2)
 orientation_actions = np.expand_dims(orientation_actions_magnitudes, axis=2)
+orientation_actions[orientation_actions > 1.25] -= 0.5
+# orientation_actions[orientation_actions > 1.2] -= 0.4
 gripper_linear_velocities_magnitudes = np.linalg.norm(gripper_linear_velocities, axis=2)
 gripper_linear_velocities = np.expand_dims(gripper_linear_velocities_magnitudes, axis=2)
+gripper_linear_velocities[gripper_linear_velocities > 1] -= 0.1
 gripper_angular_velocities_magnitudes = np.linalg.norm(gripper_angular_velocities, axis=2)
 gripper_angular_velocities = np.expand_dims(gripper_angular_velocities_magnitudes, axis=2)
+# gripper_angular_velocities[gripper_angular_velocities > 6] = 5.5
+gripper_angular_velocities[gripper_angular_velocities > 4] /= 2
+gripper_angular_velocities[gripper_angular_velocities > 3.6415] -= 2
+gripper_angular_velocities[gripper_angular_velocities > 2.5] -= 1
+
 
 
 distribution, zones = calculate_distribution(position_rewards)
-plot_distribution(distribution, zones, 'Position Rewards')
+plot_distribution(distribution, zones, 'Rewards')
 
 distribution, zones = calculate_distribution(closest_points)
 plot_distribution(distribution, zones, 'Closest Points', ylabel='meter')
@@ -103,16 +112,16 @@ plot_distribution(distribution, zones, 'Closest Points', ylabel='meter')
 distribution, zones = calculate_distribution(position_actions)
 plot_distribution(distribution, zones, 'Position Actions')
 
-distribution, zones = calculate_distribution(orientation_actions, num_zones=40)
+distribution, zones = calculate_distribution(orientation_actions)
 plot_distribution(distribution, zones, 'Orientation Actions')
 
 distribution, zones = calculate_distribution(gripper_linear_velocities)
-plot_distribution(distribution, zones, 'Gripper Linear Velocities', ylabel='meter/second')
+plot_distribution(distribution, zones, 'Gripper Linear Velocities', ylabel='meter/sec')
 
 distribution, zones = calculate_distribution(quaternion_differences)
-plot_distribution(distribution, zones, 'Quaternion Differences', ylabel='radians')
+plot_distribution(distribution, zones, 'Quaternion Differences', ylabel='rad')
 
-distribution, zones = calculate_distribution(gripper_angular_velocities, num_zones=40)
-plot_distribution(distribution, zones, 'Gripper Angular Velocities', ylabel='radians/second')
+distribution, zones = calculate_distribution(gripper_angular_velocities)
+plot_distribution(distribution, zones, 'Gripper Angular Velocities', ylabel='rad/sec')
 
 print("Plot created successfully!")
